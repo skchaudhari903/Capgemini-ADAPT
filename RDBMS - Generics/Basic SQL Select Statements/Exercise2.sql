41bc4370d3d5647d3baea58316de55b05e610746
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-07-22 21:00:53 +0530

    Push Basic SQL Select Statements

--- RDBMS - Generics/Basic SQL Select Statements/Exercise2.sql
+++ RDBMS - Generics/Basic SQL Select Statements/Exercise2.sql
@@ -1,5 +1,8 @@
 --Display details (account no, primary customer id, current balance and a/c start date) of active SB accounts in the order of latest start date (first) to earliest start date (last).
 
+alter session set current_schema=bank;
+SET heading OFF;
+
 SELECT ACCOUNT_NO, PRIMARY_CUST_ID, CURR_BAL_AMT, START_DATE
 FROM BANK_SB_ACCOUNT
 WHERE ACC_STATUS = 'Active'
--- RDBMS - Generics/Basic SQL Select Statements/Exercise3.sql
+++ RDBMS - Generics/Basic SQL Select Statements/Exercise3.sql
@@ -1,5 +1,9 @@
 --Display bank employee designation, id, name and gender of those employees who have a manager, sorted in descending order of designation and ascending order of employee name.
 
+alter session set current_schema=bank;
+SET heading OFF;
+
 SELECT DESIGNATION, BANK_EMP_ID, EMP_NAME, EMP_SEX
 FROM BANK_EMPLOYEE
-WHERE MANAGER_ID IS NOT NULLORDER BY DESIGNATION DESC, EMP_NAME ASC;  
+WHERE MANAGER_ID IS NOT NULL
+ORDER BY DESIGNATION DESC, EMP_NAME ASC;  
