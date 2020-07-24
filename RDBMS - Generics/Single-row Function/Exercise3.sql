3a6f6d85bbd12f710d2ae3f331e6db83a8fef873
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-07-24 21:00:15 +0530

    Push Single-row Function

--- RDBMS - Generics/Single-row Function/Exercise3.sql
+++ RDBMS - Generics/Single-row Function/Exercise3.sql
@@ -1,4 +1,13 @@
-
+--Write a query to show transaction date, account no., description, amount and transaction type (show 'Credit' for 'CR' and 'Debit' for 'DB') [change the column heading to "trans_type"] for all transaction amounts > 10000 other than RD installments, in the ascending order of transaction type and descending order of transaction date
 
 alter session set current_schema=bank;
 SET heading OFF;
+
+SELECT TRANS_DT, TRANS_ACC_NO, TRANS_DESC, TRANS_AMT,
+CASE 
+	WHEN TRANS_TYPE = 'CR' THEN 'Credit'
+	ELSE 'Debit'
+END AS "TRANS_TYPE"
+FROM BANK_TRANSACTION
+WHERE TRANS_DESC NOT LIKE 'RD INST%' AND TRANS_AMT > 10000
+ORDER BY TRANS_TYPE ASC, TRANS_DT DESC;
--- RDBMS - Generics/Single-row Function/Exercise4.sql
+++ RDBMS - Generics/Single-row Function/Exercise4.sql
@@ -1,5 +1,14 @@
-
+--Display account no., customer id, initial amount in terms of lakhs (rounded off to 2 decimals) [change the column heading to 'INITIALAMTIN_LAKHS'], amount category(amt < 50000 show as 'Low', amt > 50 lakhs as 'High', otherwise, 'Medium') [change the column heading to "AMOUNT_CATEGORY"], start date, maturity date (use term in months to calculate it) [change the column heading to "MATURITY_DATE"] of all 'ACTIVE' (upper case) FD accounts which were started after year 2004.
 
 alter session set current_schema=bank;
 SET heading OFF;
 
+SELECT ACCOUNT_NO, CUST_ID, ROUND(INITIAL_AMT/100000, 2) AS "INITIALAMTIN_LAKHS"
+CASE
+	WHEN INITIAL_AMT < 50000 THEN 'Low'
+	WHEN INITIAL_AMT > 5000000 THEN 'High'
+	ELSE 'Medium'
+END AS "AMOUNT_CATEGORY",
+START_DT, ADD_MONTHS(START_DT, FD_TERM_MNTH) AS "MATURITY_DATE"
+FROM BANK_FD_ACCOUNT
+WHERE UPPER(ACC_STATUS) = 'ACTIVE' AND TO_CHAR(START_DT, 'YYYY') > 2004;
